522683b778d872c56b4f6fccf25d386af5c3c6ad
Marc Krivitzky <marckrivitzky@gmail.com>
2022-03-09 13:14:48 -0500

    second commit

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,6 @@
 import React, { PureComponent} from "react";
 import { Navbar, NavItem} from "react-bootstrap";
-import { HashLink} from "react-router-hash-link";
+import { HashLink as Link } from 'react-router-hash-link';
 import { withRouter } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Marc from '../assets/marc2.png';
